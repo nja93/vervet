f7bb7deda64f95d8012bda95b457dfc440f171f3
nja93 <njanjalchege@gmail.com>
2023-11-03 15:15:19 +0300

    trying ti switch branches, forced to push

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,7 @@
-import { TFeed } from "@/lib/db/types";
-
-export default async function Home() {
-  const res = await fetch(
-    `${process.env.NEXTAUTH_URL}/${process.env.NEXT_PUBLIC_API_PATH}/feeds`
-  );
-  const feeds: TFeed[] = await res.json();
-
+export default function Home() {
   return (
-    <ul>
-      {feeds.map((feed) => (
-        <li key={feed.id}>{feed.title}</li>
-      ))}
-    </ul>
-  );
+    <main>
+      Welcome to Vervet Alerts
+    </main>
+  )
 }
