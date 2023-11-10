0dd546e45e3c2e38ad1566aaf6c3e5a2730c5899
Wakadaisho <paulwahome9@gmail.com>
2023-11-10 23:06:09 +0300

    Attempt fix

--- app/api/v1/templates/user/route.ts
+++ app/api/v1/templates/user/route.ts
@@ -1,7 +1,9 @@
 import db from "@/lib/db";
+import { template, userTemplate } from "@/lib/db/schema";
 import { getLimitOffset, resourceNotFound } from "@/lib/utils/api";
 import { getUserToken } from "@/lib/utils/authOptions";
 import { getCount } from "@/lib/utils/db";
+import { createInsertSchema } from "drizzle-zod";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
@@ -48,4 +50,42 @@ export async function GET(req: NextRequest) {
     }));
 
   return NextResponse.json({ templates: userTemplates });
+}
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  const validator = createInsertSchema(template).safeParse(body);
+
+  if (validator.success === false) {
+    return NextResponse.json(validator.error, { status: 400 });
+  }
+
+  let temp;
+  const token = await getUserToken(req);
+  const userId = token?.sub;
+
+  if (!userId) {
+    return NextResponse.json({ error: "Unauthorized Access" }, { status: 404 });
+  }
+
+  // Confirm user exists
+  const userExists = await getCount("user", "id", userId);
+
+  if (!userExists) {
+    return resourceNotFound("user", userId);
+  }
+
+  await db.transaction(async (tx) => {
+    const [_template] = await tx
+      .insert(template)
+      .values({ name: body.name, content: body.content })
+      .returning({ id: template.id });
+
+    await tx
+      .insert(userTemplate)
+      .values({ userId: userId, templateId: _template.id });
+
+    temp = _template;
+  });
+  return NextResponse.json(temp, { status: 201 });
 }
